69b50049032b75949b763cecdbf645e330556657
hebert villafuerte <31359121+HebertVillafuerte@users.noreply.github.com>
2018-07-21 22:13:16 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,83 @@
+new Vue({
+  el:'#aplicacion',
+  data:{
+    cambiar:'ulcuadricula',
+    modal:'ocultarmodal',
+    ruta:'',
+    title2:'',
+    articulos:[{
+      "title":"Catedral Arequipa",
+      "description":"Hermosa vista de la catedral arequipa",
+      "image":{
+        "imagelista":"1.jpg",
+        "imagecuadricula":"1.jpg"
+      }
+    },
+    {
+      "title":"Palmeras y playa",
+      "description":"Hermosa playa cubierta de palmeras elegantes",
+      "image":{
+        "imagelista":"2.jpg",
+        "imagecuadricula":"2.jpg"
+      }
+    },
+    {
+      "title":"Bellisima torre ifel -Paris",
+      "description":"Bonito atardecer de la torre ifel",
+      "image":{
+        "imagelista":"3.jpg",
+        "imagecuadricula":"3.jpg"
+      }
+    },
+    {
+      "title":"Patagonia y sus Montañas",
+      "description":"Un bonito lugar para ir de incursion",
+      "image":{
+        "imagelista":"4.jpg",
+        "imagecuadricula":"4.jpg"
+      }
+    },
+    {
+      "title":"Mega ciudad",
+      "description":"Mega ciudad con rascacielos enormes",
+      "image":{
+        "imagelista":"5.jpg",
+        "imagecuadricula":"5.jpg"
+      }
+    },
+    {
+      "title":"Buena vista de la muralla China",
+      "description":"Una hermosa maravilla del mundo",
+      "image":{
+        "imagelista":"6.jpg",
+        "imagecuadricula":"6.jpg"
+      }
+    },
+    {
+      "title":"Bellisima iluminacion",
+      "description":"apreciar de noche la ciudad es hermoso",
+      "image":{
+        "imagelista":"7.jpg",
+        "imagecuadricula":"7.jpg"
+      }
+    },
+    {
+      "title":"Lago cristalino",
+      "description":"Bonito lugar para navegar",
+      "image":{
+        "imagelista":"8.jpg",
+        "imagecuadricula":"8.jpg"
+      }
+    },
+    {
+      "title":"Llega la noche",
+      "description":"hermosa la naturales",
+      "image":{
+        "imagelista":"9.jpg",
+        "imagecuadricula":"9.jpg"
+      }
+    },
+
+  ]
+  }
+})
